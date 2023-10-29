b91422514148d5836879c1e8429892e70c22da4c
HamiltonFRJ <hamilton.franc50@gmail.com>
2023-10-29 17:45:26 -0300

    starting with modules/reading the page and finding the number of produts of it

---
+++ scrapping.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+import pandas as pd
+import math
+
+url = 'https://www.kabum.com.br/promocao/MENU_PCGAMER'
+
+headers = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36 OPR/102.0.0.0"}
+
+site = requests.get(url, headers=headers)
+soup = BeautifulSoup(site.content, 'html.parser')
+qtd_itens = soup.find('div', id='listingCount').get_text().strip()
+
+index = qtd_itens.find(' ')
+qtd = qtd_itens[:index]
